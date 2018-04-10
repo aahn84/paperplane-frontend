1f3acd6014ad47d814b495375a4a9f7460c12152
Angela Ahn <aahn84@gmail.com>
2018-04-10 00:07:50 -0700

    remove old action creators and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,55 +1,7 @@
 import { combineReducers } from 'redux';
 import {
-  SET_WELCOME_VIEW,
-  HIDE_WELCOME_VIEW,
-  // SHOW_LOGIN_FORM,
-  // HIDE_LOGIN_FORM,
-  // SHOW_SIGNUP_FORM,
-  // HIDE_SIGNUP_FORM,
   TRIPS_RECEIVED,
 } from '../actions';
-
-function welcomeView(state = null, action) {
-  switch(action.type) {
-    case SET_WELCOME_VIEW: {
-      return action.viewName;
-    }
-    case HIDE_WELCOME_VIEW: {
-      return null;
-    }
-    default: {
-      return state;
-    }
-  }
-}
-
-// function loginView(state = '', action) {
-//   switch(action.type) {
-//     case SHOW_LOGIN_FORM: {
-//       return action.viewName;
-//     }
-//     case HIDE_LOGIN_FORM: {
-//       return '';
-//     }
-//     default: {
-//       return state;
-//     }
-//   }
-// }
-//
-// function signupView(state = '', action) {
-//   switch(action.type) {
-//     case SHOW_SIGNUP_FORM: {
-//       return action.viewName;
-//     }
-//     case HIDE_SIGNUP_FORM: {
-//       return '';
-//     }
-//     default: {
-//       return state;
-//     }
-//   }
-// }
 
 // function user_info(state = {}, action) {
 //   switch(action.type) {
