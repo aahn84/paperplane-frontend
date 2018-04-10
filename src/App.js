6e7732d7f1bf9ecdcb04c9d26edd720acbdf1387
Angela Ahn <aahn84@gmail.com>
2018-04-09 19:34:46 -0700

    change bottom nav opacity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Welcome from './components/Welcome/Welcome';
 import MyTrips from './components/MyTrips/MyTrips';
 import TripFlights from './components/TripFlights/TripFlights';
@@ -9,16 +11,27 @@ import AddTrip from './components/MyTrips/AddTrip';
 import AddFlight from './components/TripFlights/AddFlight';
 
 class App extends Component {
+  // componentDidMount() {
+  //   if (this.props.user_id) this.props.setUserInfo(this.props.user_id);
+  // }
+
   render() {
     return (
-      <div className="App">
-        {/* <Welcome /> */}
-        {/* <MyTrips /> */}
-        {/* <TripFlights /> */}
-        {/* <FlightDetails /> */}
-        {/* <AddTrip /> */}
-        <AddFlight />
-      </div>
+      <Router>
+        <div className="App">
+          {/* <Welcome /> */}
+          <Switch>
+            <Route path="/" component={ MyTrips } />
+            {/* <MyTrips /> */}
+            {/* <TripFlights /> */}
+            {/* <FlightDetails /> */}
+            {/* <AddTrip /> */}
+            {/* <AddFlight /> */}
+
+            {/* <Route path="/" component={ ProductList }/> */}
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
